92bc4013f2e3dfbc45e7b210768848ced0b82139
PY-GZKY <341796767@qq.com>
2022-02-28 16:07:00 +0800

    Test: 测试用例通过

--- tests/test_single.py
+++ tests/test_single.py
@@ -14,24 +14,24 @@ def setup_function():
         username='admin',
         password='sanmaoyou_admin_',
         database='arrow测试库',
-        collection='arrow测试表'
+        # collection='arrow测试表_200000'
     )
 
 
 def test_to_csv():
-    result_ = M.to_csv(folder_path="_csv",is_block=True,block_size=100000)
+    result_ = M.to_csv(folder_path="_csv",is_block=True, block_size=50000)
     print(result_)
     assert "successfully" in result_
 
 
 # def test_to_excel():
-#     result_ = M.to_excel(folder_path="_excel", is_block=True, block_size=10000, mode='sheet',ignore_error=True)
+#     result_ = M.to_excel(folder_path="_excel", is_block=True, block_size=50000, mode='xlsx', ignore_error=True)
 #     print(result_)
 #     assert "successfully" in result_
 
 
 # def test_to_json():
-#     result_ = M.to_json(folder_path="./_json", is_block=True, block_size=10000,)
+#     result_ = M.to_json(folder_path="./_json", is_block=False, block_size=50000)
 #     print(result_)
 #     assert "successfully" in result_
 
